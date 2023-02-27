444a5432d5c65a28c94503261e31f742b5203739
Saba <sabanafchigar@gmail.com>
2023-02-27 12:20:25 +0330

    Add files via upload

---
+++ indexOf.js
@@ -0,0 +1,7 @@
+let data=[
+    "saba","nafchigar",31,true,"nafchigar","saba"
+]
+console.log(data.lastIndexOf("saba")); // 5
+console.log(data.indexOf("saba")); // 0
+console.log(data.indexOf("nafchigar",2)); // 4
+console.log(data.indexOf("turaj")); // -1
